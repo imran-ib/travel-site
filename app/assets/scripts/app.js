8d4971541020e6618e06f9a84a1bac9c9cb5a45d
Imran <imran-ib@outlook.com>
2017-12-31 17:51:47 +0300

    completd the reveal on scrool

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,6 +1,10 @@
 import MobileManu from './modules/MobileManu';
+import RevealOnScroll from './modules/RevealOnScroll';
+import $  from 'jquery';
 
 var mobileManu = new MobileManu();
+new RevealOnScroll($(".feature-item" ), "85%");
+new RevealOnScroll($(".testimonial"), "60%");
 
 
 
